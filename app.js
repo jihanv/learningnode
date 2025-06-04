b2b355ba17ecc41aef9f994a8de1b33a6c5dd0a0
jihanv <jihanv@gmail.com>
2025-06-04 20:19:56 +0900

    Add post function

--- app.js
+++ app.js
@@ -1,74 +1,86 @@
-const express = require("express")
-const morgan = require("morgan")
-const mongoose = require("mongoose")
-const Blog = require("./models/blog")
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
 // express app
-const app = express()
+const app = express();
 
 //register for view engine
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 //Connect to mongodb
-const dbURI = "mongodb+srv://jinja:osiduf0rw89usAS@nodetut.xxl2kuo.mongodb.net/node-tuts?retryWrites=true&w=majority&appName=nodetut"
-mongoose.connect(dbURI)
-    .then(() => { app.listen(3000) })
-    .catch((err) => { console.log("ERROR") })
+const dbURI =
+  "mongodb+srv://jinja:osiduf0rw89usAS@nodetut.xxl2kuo.mongodb.net/node-tuts?retryWrites=true&w=majority&appName=nodetut";
+mongoose
+  .connect(dbURI)
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log("ERROR");
+  });
 
 //middleware to give access to static files conveniently. Anything in the "public" folder can now be accessed
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 // middleware that logs information for you
-app.use(morgan('dev'))
+app.use(morgan("dev"));
+
+//Middleware to help parse post request data
+app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-    res.locals.path = req.path;
-    next();
-})
+  res.locals.path = req.path;
+  next();
+});
 
 app.get("/", (req, res) => {
-    res.redirect("/blogs")
-})
+  res.redirect("/blogs");
+});
 
 app.get("/about", (req, res) => {
-    res.render("about", { title: "About" })
-})
+  res.render("about", { title: "About" });
+});
 
 // app.use((req, res, next) => {
 //     console.log('in the next middleware');
 //     next();
 // })
 
-
 //These are the blog routes
 
 //Gets the things from blogs collection from that database
-app.get("/blogs", (req, res)=>{
-    Blog.find().sort({createdAt: -1})
+// Sorts it in descending order
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
     .then((result) => {
-        res.render("index", {title: "All Blogs", blogs: result})
-    })
-    .catch((err)=>{
-        console.log("Error!")
-    })
-})
-
-//Find blog by id
-app.get("/single-blog", (req, res) => {
-    Blog.findById("6832fac69f813d0a6950f84f")
-    .then((result) => {
-        res.send(result)
+      res.render("index", { title: "All Blogs", blogs: result });
     })
     .catch((err) => {
-        console.log(err)
-    })
-})
+      console.log("Error!");
+    });
+});
+
+//Post blog
+app.post("/blogs", (req, res) => {
+  const blog = new Blog(req.body)
+
+  blog.save()
+  .then(result => {
+    res.redirect("/blogs")
+  })
+  .catch(err => {
+    console.log(err)
+  })
+});
 
 app.get("/blogs/create", (req, res) => {
-    res.render("create", { title: "Create a new blog" })
-})
+  res.render("create", { title: "Create a new blog" });
+});
 
 // page does not exist
 app.use((req, res) => {
-    res.status(404).render("404", { title: "404" })
-})
+  res.status(404).render("404", { title: "404" });
+});
