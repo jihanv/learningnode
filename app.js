52e9469ddea12e2a7b78488463c355297e9e077d
jihanv <hedjmny@gmail.com>
2025-06-04 20:51:35 +0900

    Add route parameters

--- app.js
+++ app.js
@@ -76,6 +76,18 @@ app.post("/blogs", (req, res) => {
   })
 });
 
+app.get("/blogs/:id", (req,res) => {
+    const id = req.params.id
+    Blog.findById(id)
+    .then(result => {
+        res.render("details", {blog: result, title: "Blog Details"})
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
+})
+
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create a new blog" });
 });
